9a23c1dd8bd094ca22955bec2b2e4eb8bf006d75
David McKoskey <david@mckoskey.com>
2015-04-05 00:02:30 -0500

    Revised letter, added footer and batch file.

--- letter.pl
+++ letter.pl
@@ -12,16 +12,27 @@ use Carp;
 use English;
 use IO::File;
 use Utilities;
+use Getopt::Long;
 use File::Basename;
+
+my $quote = undef;
+
+GetOptions
+(
+    'q=s' => \$quote,
+);
 
 if($#ARGV < 0) { Syntax(); }
 
 my $util = new Utilities();
 my $today = $util->get_print_date();
 
-my $filename = shift @ARGV;
+if($util->test_var($quote))
+    { $quote = "\\hspace*{2in} ``" . $quote . ".''"; }
+else
+    { $quote = "\\hspace*{2in} ``This is a zither.'' -- David McKoskey"; }
 
-my @extensions = qw(.tex);
+my $filename = shift @ARGV;
 
 if($filename =~ /\*/)
 {
@@ -42,8 +53,8 @@ if($filename !~ /\.tex/)
 
 if(-e $filename)
 {
-    print "File \"" . $filename . "\" exists, skipping...\n";
-    next;
+    print "File \"" . $filename . "\" exists, exiting.\n";
+    exit 0;
 }
 
 my $file = IO::File->new($filename, ">") or croak "Unable to open file \"" . $filename . "\": " . $OS_ERROR;
@@ -55,6 +66,7 @@ print $file "\n";
 print $file "\\documentclass[letterpaper, 11pt]{letter}\n";
 print $file "\n";
 print $file "\\usepackage{pstricks}\n";
+print $file "\\usepackage{fancyhdr}\n";
 print $file "\n";
 print $file "%%%%% Document Setup %%%%%\n";
 print $file "\\setlength{\\topmargin}{0.5in}        % top margins\n";
@@ -64,10 +76,15 @@ print $file "\\setlength{\\topskip}{0in}\n";
 print $file "\\setlength{\\oddsidemargin}{0in}      % side margins\n";
 print $file "\\setlength{\\evensidemargin}{0in}\n";
 print $file "\\setlength{\\textwidth}{6.5in}        % text width\n";
-print $file "\\setlength{\\textheight}{9in}         % text height\n";
+print $file "\\setlength{\\textheight}{8.5in}       % text height\n";
 print $file "% \\setlength{\\footskip}{1in}           % bottom margins\n";
-print $file "\\setlength{\\parindent}{0in}\n";
-print $file "% \\pagestyle{empty}                    % no page numbers\n";
+print $file "% \\pagestyle{empty}                    % no header and footer\n";
+print $file "\\pagestyle{fancy}                    % add header or footer (or both)\n";
+print $file "\n";
+print $file "%%%%% Document Font Family %%%%%\n";
+print $file "\\renewcommand{\\familydefault}{\\rmdefault} % Roman / serif\n";
+print $file "% \\renewcommand{\\familydefault}{\\sfdefault} % Sans serif\n";
+print $file "% \\renewcommand{\\familydefault}{\\ttdefault} % Typeface\n";
 print $file "\n";
 print $file "%%%%% Notary Public %%%%%\n";
 print $file "\\newcommand\\notarypublic{%\n";
@@ -76,10 +93,10 @@ print $file "\\hline\n";
 print $file "\\multicolumn{2}{|c|}{Sworn before me in \\underline{\\hspace*{2.25in}}}\\\\\n";
 print $file "\\multicolumn{2}{|c|}{on the \\underline{\\hspace*{2cm}} day of \\underline{\\hspace*{3cm}}, 20\\underline{\\hspace*{1cm}}}\\\\\n";
 print $file "\\hline\n";
-print $file "Signature & Seal\\\\\n";
-print $file "& \\\\\n";
-print $file "& \\\\\n";
-print $file "& \\\\\n";
+print $file "Signature & Seal \\\\\n";
+print $file "          &      \\\\\n";
+print $file "          &      \\\\\n";
+print $file "          &      \\\\\n";
 print $file "\\hline\n";
 print $file "\\multicolumn{2}{|l|}{Commission Expires:}\\\\\n";
 print $file "\\hline\n";
@@ -95,11 +112,9 @@ print $file "    \\xdef\\frowny{\\facewith\\frown}\n";
 print $file "    \\xdef\\smiley{\\facewith\\smile}\n";
 print $file "\\endgroup\n";
 print $file "\n";
-print $file "\\newcommand{\\dollaramount}{\\\$0.00 } % Keep a space at the right edge\n";
-print $file "\n";
 print $file "\\begin{document}\n";
 print $file "\n";
-print $file "%%%%% Title Page %%%%%\n";
+print $file "%%%%% Title Information %%%%%\n";
 print $file "\\date{" . $util->get_print_date() . "}\n";
 print $file "\\begin{letter}{Mr. Somebody \\\\ 1234 Pleasant St.  \\\\ St. Paul, MN  55101}\n";
 print $file "\\address{David McKoskey \\\\ 1976 Pinehurst Ave. \\\\ St. Paul, MN 55116}\n";
@@ -108,25 +123,40 @@ print $file "\\opening{Dear Sir,}\n";
 print $file "% \\signature{David McKoskey \\\\[1cm] \\notarypublic}\n";
 print $file "\\signature{David McKoskey}\n";
 print $file "\n";
-print $file "\\raggedright\n";
-print $file "\\raggedbottom\n";
+print $file "%%%%% Header and Footer Information %%%%%\n";
+print $file "\\lhead{}\n";
+print $file "\\chead{}\n";
+print $file "\\rhead{}\n";
+print $file "\\lfoot{}\n";
+print $file "\\cfoot{" . $quote . "}\n";
+print $file "\\rfoot{}\n";
+print $file "\\renewcommand{\\headrulewidth}{0pt}   %   No line by default.  Change to 0.4pt for line.\n";
+print $file "\\renewcommand{\\footrulewidth}{0.4pt} % Thin line by default.\n";
 print $file "\n";
-print $file "%%%%%%%%%%%%%%%%% LETTER BODY  %%%%%%%%%%%%%%%%\n";
+print $file "%%%%% Page Specifications %%%%%\n";
+print $file "\\thispagestyle{fancy}\n";
+print $file "\\raggedbottom\n";
+print $file "\\raggedright\n";
+print $file "\n";
+print $file "%%%%%%%%%%%%%%%%% LETTER BODY %%%%%%%%%%%%%%%%\n";
 print $file "\n";
 print $file "\n";
 print $file "\\closing{Sincerely,}\n";
-print $file "% \\encl{Stuff For You}\n";
+print $file "% \\encl{Stuff for You}\n";
 print $file "% \\cc{Dewey \\\\ Cheatham \\\\ Howe}\n";
 print $file "\\end{letter}\n";
 print $file "\\end{document}\n";
 
 close($file);
 
+my @extensions = qw(.tex);
 my($name, $path, $extension) = fileparse($filename, @extensions);
 
 my $r_bat = IO::File->new("r.bat", ">") or croak "Unable to write r.bat: " . $OS_ERROR;
 
 print $r_bat "\@echo off\n";
+print $r_bat "\n";
+print $r_bat ":* cls\n";
 print $r_bat "\n";
 print $r_bat "pdflatex " . $name . ".tex\n";
 print $r_bat "pdflatex " . $name . ".tex\n";
@@ -142,9 +172,10 @@ close $r_bat;
 sub Syntax
 {
     print "\n";
-    print "\tSyntax: letter <filename(s)>\n";
+    print "    Syntax: letter [-q] <filename(s)>\n";
+    print "        -q <quote at the bottom of the page>\n";
     print "\n";
-    print "\tNote: do not use wildcards\n";
+    print "    Note: do not use wildcards\n";
     print "\n";
 
     exit -1;
